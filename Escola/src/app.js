30d682c948d7f57c9a4c8df1ef52e89a0a20a416
TheuTheuutra <105327246+TheuTheuutra@users.noreply.github.com>
2023-05-12 01:17:52 -0300

    commit do projeto

---
+++ Escola/src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+const rota_aluno = require('./routes/rota_aluno.js');
+const rota_turma = require('./routes/rota_turma.js');
+//const Post = require('./models/Post');
+//carregando o cabeçalho do html em outras páginas
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//arquivos estáticos
+app.use('/public/css', express.static('public/css'));
+app.use('/public/js', express.static('public/js'));
+app.use('/public/img', express.static('public/img'));
+//Remanejando Rotas de aluno
+app.use('/rota_aluno', rota_aluno);
+//Remanejando Rotas de turma
+app.use('/rota_turma', rota_turma);
+const PORT = 8081;
+app.listen(PORT,()=>{
+ console.log("Servidor Rodando");
+});
