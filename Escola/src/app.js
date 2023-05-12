29e414fb0c32630c6ba9f9159a71859363d777ae
TheuTheuutra <105327246+TheuTheuutra@users.noreply.github.com>
2023-05-12 20:14:58 -0300

    commit do projeto 2

--- Escola/src/app.js
+++ Escola/src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const rota_aluno = require('./routes/rota_aluno.js');
 const rota_turma = require('./routes/rota_turma.js');
+const admin = require('./routes/admin.js');
 //const Post = require('./models/Post');
 //carregando o cabeçalho do html em outras páginas
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,9 +12,13 @@ app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 //arquivos estáticos
-app.use('/public/css', express.static('public/css'));
-app.use('/public/js', express.static('public/js'));
+app.use('/public/css', express.static('public/bootstrap/css'));
+app.use('/public/js', express.static('public/bootstrap/js'));
 app.use('/public/img', express.static('public/img'));
+
+//Remanejando Rotas admin
+app.use('/', admin);
+
 //Remanejando Rotas de aluno
 app.use('/rota_aluno', rota_aluno);
 //Remanejando Rotas de turma
